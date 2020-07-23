72fee5d7f61dedf00a67323b400787300dfb753f
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-07-22 17:47:28 -0700

    update createLambdaRole

---
+++ src/aws/lambda/createLambdaRole.js
@@ -0,0 +1,58 @@
+const {
+  asyncCreateLambdaRole,
+  attachRolePolicy,
+  asyncAttachRolePolicy,
+} = require('./index');
+
+const createLambdaRole = async () => {
+  console.log('Creating Lambda role...');
+
+  const ROLE = 'ROLE_LAMBDA';
+  const myPolicy = {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Principal: {
+          Service: 'lambda.amazonaws.com',
+        },
+        Action: 'sts:AssumeRole',
+      },
+    ],
+  };
+
+  const createParams = {
+    AssumeRolePolicyDocument: JSON.stringify(myPolicy),
+    RoleName: ROLE,
+  };
+
+  const lambdaPolicyParams = {
+    PolicyArn: 'arn:aws:iam::aws:policy/service-role/AWSLambdaRole',
+    RoleName: ROLE,
+  };
+
+  try {
+    const createResponse = await asyncCreateLambdaRole(createParams);
+    console.log('Successfully created Lambda role.', createResponse);
+
+    const attachResponse = await asyncAttachRolePolicy(lambdaPolicyParams);
+    console.log('Successfully attached policy.', attachResponse);
+  } catch (error) {
+    console.log('Could not create role.', error);
+  }
+
+  // iam.createRole(createParams, function (err, data) {
+  //   if (err) {
+  //     console.log(err, err.stack);
+  //   } else {
+  //     console.log('Role ARN is', data.Role.Arn);
+  //     iam.attachRolePolicy(lambdaPolicyParams, function (err, data) {
+  //       if (err) {
+  //         console.log(err, err.stack);
+  //       } else {
+  //         console.log('AWSLambdaRole policy attached');
+  //       }
+  //     });
+  //   }
+  // });
+};
--- src/aws/lambda/index.js
+++ src/aws/lambda/index.js
@@ -1,14 +1,20 @@
+// @TODO: Move IAM out of here
+
 const { promisify } = require('util');
 const AWS = require('aws-sdk/global');
 const Lambda = require('aws-sdk/clients/lambda');
+const awsIAM = require('aws-sdk/clients/iam');
 
 AWS.config.update({ region: 'us-east-1' });
 const lambda = new Lambda();
+const iam = new awsIAM({ apiVersion: '2010-05-08' });
 
 const asyncAddPermission = promisify(lambda.addPermission.bind(lambda));
+const asyncCreateLambdaRole = promisify(iam.attachRolePolicy.bind(iam));
 const asyncCreateLambdaFunction = promisify(lambda.createFunction.bind(lambda));
 
 module.exports = {
   asyncAddPermission,
+  asyncCreateLambdaRole,
   asyncCreateLambdaFunction,
 };
