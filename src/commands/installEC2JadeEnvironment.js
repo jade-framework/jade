943c06f4bbd3344438dcb1db56ed848adde84f0a
edmondtam1 <edmondtam2@gmail.com>
2020-07-24 08:45:05 +0800

    Server settings done

--- src/commands/installEC2JadeEnvironment.js
+++ src/commands/installEC2JadeEnvironment.js
@@ -28,19 +28,82 @@ const nodeCommands = [
 
 const gitCommands = [
   "sudo yum install git -y",
-  // github auth token
-  // git clone their repo
-  "git clone https://github.com/JeremyCrichton/gatsby-test",
+  "git clone https://github.com/jade-framework/gatsby-blog",
 ];
 
-const buildCommands = ["cd gatsby-test", "yarn install", "yarn build"];
+const buildCommands = ["cd gatsby-blog", "yarn install", "yarn build"];
 
 const webhookCommands = [
   "sudo amazon-linux-extras install nginx1 -y",
+  "sudo mv /home/ec2-user/server/sysmon.conf /etc/nginx/conf.d/sysmon.conf",
   "sudo systemctl start nginx",
+  "node /home/ec2-user/server/server.js &",
 ];
 
-const deployCommands = ["aws s3 sync public s3://bucket-sounds-cool-to-me"];
+const deployCommands = (bucketName) => {
+  return [`aws s3 sync public s3://${bucketName}`];
+};
+
+const sendCommands = (conn, bucketName) => {
+  conn.shell((err, stream) => {
+    if (err) throw err;
+    stream
+      .on("close", () => {
+        console.log("Stream :: close");
+        conn.end();
+      })
+      .on("data", (data) => {
+        console.log("OUTPUT: " + data);
+      });
+    stream.end(
+      [
+        ...linuxCommands,
+        ...nodeCommands,
+        ...gitCommands,
+        ...buildCommands,
+        ...webhookCommands,
+        ...deployCommands(bucketName),
+        "exit\n",
+      ].join("\n")
+    );
+  });
+};
+
+const sendFiles = async (conn) => {
+  const serverDir = "/home/ec2-user/server";
+  conn.sftp((err, sftp) => {
+    if (err) throw err;
+    sftp.mkdir(serverDir, {}, (err) => {
+      if (err) throw err;
+
+      sftp.fastPut(
+        join(hostDirectory, "src", "server", "server.js"),
+        join(serverDir, "server.js"),
+        {},
+        (err) => {
+          if (err) throw err;
+          sftp.fastPut(
+            join(hostDirectory, "src", "server", "triggerBuild.js"),
+            join(serverDir, "triggerBuild.js"),
+            {},
+            (err) => {
+              if (err) throw err;
+              sftp.fastPut(
+                join(hostDirectory, "src", "server", "sysmon.conf"),
+                join(serverDir, "sysmon.conf"),
+                {},
+                (err) => {
+                  if (err) throw err;
+                  console.log("Files uploaded to EC2.");
+                }
+              );
+            }
+          );
+        }
+      );
+    });
+  });
+};
 
 const sshConnection = async (host) => {
   const conn = new Client();
@@ -52,28 +115,11 @@ const sshConnection = async (host) => {
   conn.on("ready", () => {
     connected = true;
     console.log("Client ready");
-    conn.shell((err, stream) => {
-      if (err) throw err;
-      stream
-        .on("close", () => {
-          console.log("Stream :: close");
-          conn.end();
-        })
-        .on("data", (data) => {
-          console.log("OUTPUT: " + data);
-        });
-      stream.end(
-        [
-          ...linuxCommands,
-          ...nodeCommands,
-          ...gitCommands,
-          ...buildCommands,
-          ...webhookCommands,
-          ...deployCommands,
-          "exit\n",
-        ].join("\n")
-      );
-    });
+    // Handle SFTP
+    await sendFiles(conn);
+
+    // Handle installation, build and deploy commands
+    sendCommands(conn);
   });
 
   conn.connect(host);
@@ -88,7 +134,7 @@ const sshConnection = async (host) => {
   }
 };
 
-module.exports = async function installEC2JadeEnvironment() {
+module.exports = async function installEC2JadeEnvironment(bucketName) {
   const privateKey = await readFile(join(jadePath, privateKeyFilename));
 
   try {
@@ -104,7 +150,9 @@ module.exports = async function installEC2JadeEnvironment() {
     };
 
     console.log("Beginning connection to EC2 server...");
-    await sshConnection(host);
+    await sshConnection(host, bucketName);
+    console.log("EC2 server setup successfully.");
+    return;
   } catch (err) {
     console.log(err);
   }
--- src/server/server.js
+++ src/server/server.js
@@ -1,61 +1,54 @@
 const http = require("http");
-const { promisify } = require("util");
 const triggerBuild = require("./triggerBuild");
 
 const port = 5000;
 const ec2InstanceMetadata =
   "http://169.254.169.254/latest/meta-data/local-ipv4";
 
-const get = promisify(http.get);
-
 async function start() {
-  http.get(ec2InstanceMetadata, (res) => {
-    let rawData = "";
-    res.on("data", (chunk) => {
-      rawData += chunk;
-    });
-    res.on("end", () => {
-      try {
-        const parsedData = JSON.parse(rawData);
-        console.log(parsedData);
-      } catch (e) {
-        console.error(e.message);
-      }
-    });
-  });
+  try {
+    http.get(ec2InstanceMetadata, (res) => {
+      let hostname = "";
+      res.on("data", (data) => {
+        hostname += data;
+      });
 
-  const server = http.createServer((req, res) => {
-    req.on("error", (err) => {
-      console.error(err);
-      res.statusCode = 400;
-      res.end();
-    });
-    res.on("error", (err) => {
-      console.error(err);
-    });
-
-    if (req.method === "POST" && req.url === "/webhook") {
-      let body = [];
-      req
-        .on("data", (chunk) => {
-          body.push(chunk);
-        })
-        .on("end", () => {
-          body = JSON.parse(Buffer.concat(body).toString());
-          // triggerBuild(body);
-          console.log(body);
-          res.statusCode = 200;
-          res.end("Webhook successfully processed.");
+      const server = http.createServer((req, res) => {
+        req.on("error", (err) => {
+          console.error(err);
+          res.statusCode = 400;
+          res.end();
         });
-    } else {
-      res.statusCode = 404;
-      res.end();
-    }
-  });
+        res.on("error", (err) => {
+          console.error(err);
+        });
 
-  server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
-  });
+        if (req.method === "POST" && req.url === "/webhook") {
+          let body = [];
+          req
+            .on("data", (chunk) => {
+              body.push(chunk);
+            })
+            .on("end", () => {
+              body = JSON.parse(Buffer.concat(body).toString());
+              triggerBuild(body);
+              console.log(body);
+              res.statusCode = 200;
+              res.end("Webhook successfully processed.");
+            });
+        } else {
+          res.statusCode = 404;
+          res.end();
+        }
+      });
+
+      server.listen(port, hostname, () => {
+        console.log(`Server running at http://${hostname}:${port}/`);
+      });
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 start();
