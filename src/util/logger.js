9a9c03c60983a727caba299c361de9b07011bed8
Krunal Patel <kppatel727@gmail.com>
2020-08-01 09:28:07 -0700

    added jadeWarn logger

--- src/util/logger.js
+++ src/util/logger.js
@@ -4,6 +4,14 @@ const jadeErr = (text) => {
   console.log(errorEmoji.concat(` ${text}`));
 };
 
+const jadeWarn = (text) => {
+  const escape = "\x1b";
+  const yellow = "[33m";
+  const reset = "\x1b[0m";
+  console.log(`${escape}${yellow}${text}${reset}`);
+};
+
 module.exports = {
   jadeErr,
+  jadeWarn,
 };
