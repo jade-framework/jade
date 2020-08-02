58e96de2a514dee4648110684b54153d2e3e25d1
edmondtam1 <edmondtam2@gmail.com>
2020-08-02 23:21:30 +0800

    Refactoring for user commands

--- src/commands/init.js
+++ src/commands/init.js
@@ -13,19 +13,44 @@ const { createDirectory } = require("../util/fileUtils");
 const { createCloudfrontDistribution } = require("../aws/cloudfront");
 const { zipit } = require("../util/zipit");
 const { build } = require("./build");
+const inquirer = require("inquirer");
 
 const cwd = process.cwd();
 const functionName = "copyToBucket";
 const functionFile = `${functionName}.js.zip`;
 const functionHandler = `${functionName}.handler`;
 const functionDescription = `Copy a file from src to dest buckets.`;
+const prompt = inquirer.createPromptModule();
 
-const init = async () => {
-  await createDirectory(".jade", cwd);
-  const bucketName = `test-${uuid.v4()}`;
+// const init = async () => {
+//   await createDirectory(".jade", cwd);
+//   const bucketName = `test-${uuid.v4()}`;
+//   await createBuckets(bucketName);
+//   await zipit(`${functionName}.js`, `${cwd}/src/aws/lambda/${functionName}.js`);
+//   await uploadToBucket(functionFile, `${bucketName}-lambda`);
+//   const lambdaRoleResponse = await createLambdaRole("lambda-s3-role-2");
+//   setTimeout(async () => {
+//     const lambdaResponse = await createLambdaFunction(
+//       `${bucketName}-lambda`,
+//       functionFile,
+//       functionName,
+//       functionHandler,
+//       functionDescription,
+//       lambdaRoleResponse.Role.Arn
+//     );
+//     const lambdaArn = lambdaResponse.FunctionArn;
+//     await createLambdaPermission(process.env.sourceAccount, lambdaArn);
+//     await createCloudfrontDistribution(bucketName);
+//     await setBucketNotificationConfig(bucketName, lambdaArn);
+//     /*** START INIT EC2 INSTANCE HERE ***/
+//     await build(bucketName);
+//   }, 10000); // It takes time for the IAM role to be replicated through all regions and become valid
+// };
+
+const start = async (directory) => {
+  await createDirectory(".jade", directory);
+  const bucketName = `jade-${uuid.v4()}`;
   await createBuckets(bucketName);
-  await zipit(`${functionName}.js`, `${cwd}/src/aws/lambda/${functionName}.js`);
-  await uploadToBucket(functionFile, `${bucketName}-lambda`);
   const lambdaRoleResponse = await createLambdaRole("lambda-s3-role-2");
   setTimeout(async () => {
     const lambdaResponse = await createLambdaFunction(
@@ -45,5 +70,23 @@ const init = async () => {
   }, 10000); // It takes time for the IAM role to be replicated through all regions and become valid
 };
 
+const init = async (args, directory) => {
+  try {
+    const questions = [
+      {
+        message: "Hello you are a question?",
+        // filter(input, answers) {
+        //   console.log(input, answers);
+        //   return Promise.resolve(input);
+        // },
+        name: "Answers",
+      },
+    ];
+    const hi = await prompt(questions);
+    console.log("hi", hi);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 module.exports = { init };
-init();
--- src/constants/allConstants.js
+++ src/constants/allConstants.js
@@ -1,23 +1,5 @@
 const hostDirectory = process.cwd();
 
-<<<<<<< HEAD
-// const amazonMachineImageId = "ami-04122be15033aa7ec";
-const amazonMachineImageId = 'ami-0b1e2eeb33ce3d66f';
-const instanceType = 't2.micro';
-const keyPair = 'keyPair';
-const privateKeyFilename = 'jade-key-pair.pem';
-const jadeKeyPair = 'jade-key-pair';
-const securityGroup = 'securityGroup';
-const securityGroupName = 'jade-security-group';
-const ec2IamRoleName = 'jade-ec2-role';
-const ec2InstanceProfile = 'jade-ec2-instance-profile';
-const s3FullAccessPolicyArn = 'arn:aws:iam::aws:policy/AmazonS3FullAccess';
-const lambdaIamRoleName = 'lambda-s3-role-2';
-const awsLambdaExecutePolicyArn = 'arn:aws:iam::aws:policy/AWSLambdaExecute';
-const awsLambdaRolePolicyArn =
-  'arn:aws:iam::aws:policy/service-role/AWSLambdaRole';
-const lambdaFunctionName = 'copyToBucket';
-=======
 const instanceType = "t2.micro";
 const keyPair = "keyPair";
 const privateKeyFilename = "jade-key-pair.pem";
@@ -27,7 +9,11 @@ const securityGroupName = "jade-security-group";
 const ec2IamRoleName = "jade-ec2-role";
 const ec2InstanceProfile = "jade-ec2-instance-profile";
 const s3FullAccessPolicyArn = "arn:aws:iam::aws:policy/AmazonS3FullAccess";
->>>>>>> master
+const lambdaIamRoleName = "lambda-s3-role-2";
+const awsLambdaExecutePolicyArn = "arn:aws:iam::aws:policy/AWSLambdaExecute";
+const awsLambdaRolePolicyArn =
+  "arn:aws:iam::aws:policy/service-role/AWSLambdaRole";
+const lambdaFunctionName = "copyToBucket";
 
 module.exports = {
   instanceType,
