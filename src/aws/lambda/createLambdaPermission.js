ae00c1daf3b4d7d1fe417544fbe0c35196d3b1ec
Krunal Patel <kppatel727@gmail.com>
2020-08-04 17:21:29 -0700

    refactor

--- src/aws/lambda/createLambdaPermission.js
+++ src/aws/lambda/createLambdaPermission.js
@@ -1,22 +1,5 @@
 const { asyncAddPermission } = require("../awsAsyncFunctions");
 
-// const createLambdaPermission = async (sourceAccount, lambdaArn) => {
-//   const params = {
-//     Action: "lambda:InvokeFunction",
-//     FunctionName: lambdaArn,
-//     Principal: "s3.amazonaws.com",
-//     SourceAccount: sourceAccount,
-//     StatementId: `example-S3-permission`,
-//   };
-
-//   try {
-//     const response = await asyncAddPermission(params);
-//     console.log("Successfully added lambda permission.", response);
-//   } catch (error) {
-//     console.log("Error adding lambda permission.", error, error.stack);
-//   }
-// };
-
 const createLambdaPermission = async (params) => {
   try {
     const response = await asyncAddPermission(params);
