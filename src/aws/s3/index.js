30d32c4c6a32a32abc89c65a56175d5919fcdc0e
Jeremy Crichton <22128243+JeremyCrichton@users.noreply.github.com>
2020-08-27 20:31:26 -0700

    update stage bucket settings

--- src/aws/s3/index.js
+++ src/aws/s3/index.js
@@ -1,22 +1,3 @@
-// const { promisify } = require('util');
-// const S3 = require('aws-sdk/clients/s3');
-
-// const s3 = new S3({ apiVersion: '2006-03-01' });
-
-// const asyncCreateS3Bucket = promisify(s3.createBucket.bind(s3));
-// const asyncPutBucketPolicy = promisify(s3.putBucketPolicy.bind(s3));
-// const asyncUploadToBucket = promisify(s3.putObject.bind(s3));
-// const asyncPutBucketNotificationConfiguration = promisify(
-//   s3.putBucketNotificationConfiguration.bind(s3)
-// );
-
-// module.exports = {
-//   asyncCreateS3Bucket,
-//   asyncPutBucketPolicy,
-//   asyncUploadToBucket,
-//   asyncPutBucketNotificationConfiguration,
-// };
-
 const { createBucket } = require('./createBucket');
 const { createBuckets } = require('./createBuckets');
 const {
@@ -24,10 +5,12 @@ const {
 } = require('./setBucketNotificationConfig');
 const { updateBucketPolicy } = require('./updateBucketPolicy');
 const { uploadToBucket } = require('./uploadToBucket');
+const { putBucketWebsite } = require('./putBucketWebsite');
 
 module.exports = {
   createBucket,
   createBuckets,
+  putBucketWebsite,
   setBucketNotificationConfig,
   updateBucketPolicy,
   uploadToBucket,
