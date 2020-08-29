06eb3beeef477adeef64c4076fbd27b5c0b449e6
Krunal Patel <kppatel727@gmail.com>
2020-08-29 12:58:05 -0700

    test instance profile

--- test/roles.test.js
+++ test/roles.test.js
@@ -5,13 +5,15 @@ const {
   createIamRole,
   deleteIamRole,
   deleteIamGroup,
+  createInstanceProfile,
+  deleteInstanceProfile,
 } = require('../src/aws/iam');
 
 const { createIamGroup } = require('../src/aws/iam/createIamGroup');
-const { jadeLog } = require('../src/util/logger');
-
 const {
   asyncListAttachedRolePolicies,
+  asyncListAttachedGroupPolicies,
+  asyncDeleteRole,
 } = require('../src/aws/awsAsyncFunctions');
 
 const { join, readJSONFile } = require('../src/util/fileUtils');
@@ -28,6 +30,8 @@ const groupPolicies = [
   'arn:aws:iam::aws:policy/AWSLambdaFullAccess',
   'arn:aws:iam::aws:policy/CloudFrontFullAccess',
 ];
+
+const profileName = 'test-JadeInstanceProfile';
 
 describe('AWS IAM', () => {
   beforeEach(async () => {
@@ -52,13 +56,14 @@ describe('AWS IAM', () => {
       const expected = { Role: { RoleName: roleName } };
       expect(role).toMatchObject(expected);
     });
-
     test('role is not created if it exists', async () => {
       const documentPolicy = await readJSONFile(
         'ec2IamConfig',
         join(__dirname, '..', 'src', 'templates'),
       );
       await createIamRole(documentPolicy, roleName, rolePolicies);
+      let role = await roleExists(roleName);
+      expect(role).toBeTruthy();
       await createIamRole(documentPolicy, roleName, rolePolicies);
       role = await roleExists(roleName);
       expect(role).toBeTruthy();
@@ -82,8 +87,24 @@ describe('AWS IAM', () => {
         join(__dirname, '..', 'src', 'templates'),
       );
       await createIamRole(documentPolicy, roleName, rolePolicies);
+      let role = await roleExists(roleName);
+      expect(role).toBeTruthy();
       let attachedPolicies = await asyncListAttachedRolePolicies(params);
       expect(attachedPolicies).toMatchObject(expected);
+    });
+    test('role is not deleted if policies are still attached', async () => {
+      const documentPolicy = await readJSONFile(
+        'ec2IamConfig',
+        join(__dirname, '..', 'src', 'templates'),
+      );
+      await createIamRole(documentPolicy, roleName, rolePolicies);
+      let role = await roleExists(roleName);
+      expect(role).toBeTruthy();
+      expect(async () => {
+        await asyncDeleteRole({ RoleName: roleName });
+      }).rejects;
+      role = await roleExists(roleName);
+      expect(role).toBeTruthy();
     });
     test('deleting role does not throw error', async () => {
       const documentPolicy = await readJSONFile(
@@ -91,6 +112,8 @@ describe('AWS IAM', () => {
         join(__dirname, '..', 'src', 'templates'),
       );
       await createIamRole(documentPolicy, roleName, rolePolicies);
+      let role = await roleExists(roleName);
+      expect(role).toBeTruthy();
       await deleteIamRole(roleName);
       role = await roleExists(roleName);
       expect(role).toBe(false);
@@ -105,22 +128,112 @@ describe('AWS IAM', () => {
     test('group is created', async () => {
       let group = await groupExists(groupName);
       expect(group).toBe(false);
-      await createIamGroup(groupName, groupName);
-      const expected = { Group: { GroupName: groupName } };
+      await createIamGroup(groupName, groupPolicies);
       group = await groupExists(groupName);
-      expect(group).toMatchObject(expected);
+      expect(group).toBeTruthy();
     });
-    test('group is not created if it exists', async () => {});
-    test('group is deleted correctly', async () => {});
-    describe('Jade user group', () => {
-      test('Jade user group is created', async () => {});
-      test('Jade user group has right permissions', async () => {});
+    test('group is not created if it exists', async () => {
+      await createIamGroup(groupName, groupPolicies);
+      let group = await groupExists(groupName);
+      expect(group).toBeTruthy();
+      await createIamGroup(groupName, groupPolicies);
+      group = await groupExists(groupName);
+      expect(group).toBeTruthy();
+    });
+    test('group policies are attached', async () => {
+      let expected = {
+        AttachedPolicies: [
+          {
+            PolicyName: 'AmazonEC2FullAccess',
+            PolicyArn: 'arn:aws:iam::aws:policy/AmazonEC2FullAccess',
+          },
+          {
+            PolicyName: 'AWSLambdaFullAccess',
+            PolicyArn: 'arn:aws:iam::aws:policy/AWSLambdaFullAccess',
+          },
+          {
+            PolicyName: 'CloudFrontFullAccess',
+            PolicyArn: 'arn:aws:iam::aws:policy/CloudFrontFullAccess',
+          },
+        ],
+      };
+
+      await createIamGroup(groupName, groupPolicies);
+      let group = await groupExists(groupName);
+      expect(group).toBeTruthy();
+      let attachedPolicies = await asyncListAttachedGroupPolicies({
+        GroupName: groupName,
+      });
+      expect(attachedPolicies).toMatchObject(expected);
+    });
+    test('group is deleted correctly', async () => {
+      await createIamGroup(groupName, groupPolicies);
+      let group = await groupExists(groupName);
+      expect(group).toBeTruthy();
+      await deleteIamGroup(groupName);
+      group = await groupExists(groupName);
+      expect(group).toBe(false);
     });
   });
 
   describe('Instance Profile', () => {
-    test('instance profile is created', async () => {});
-    test('instance profile is not created if it exists', async () => {});
-    test('instance profile is deleted correctly', async () => {});
+    afterEach(async () => {
+      await deleteInstanceProfile(profileName, roleName);
+      await deleteIamRole(roleName);
+    });
+
+    test('instance profile is created', async () => {
+      let profile = await instanceProfileExists(profileName);
+      expect(profile).toBe(false);
+      const documentPolicy = await readJSONFile(
+        'ec2IamConfig',
+        join(__dirname, '..', 'src', 'templates'),
+      );
+      let role = await roleExists(roleName);
+      expect(role).toBe(false);
+      await createIamRole(documentPolicy, roleName, rolePolicies);
+      role = await roleExists(roleName);
+      expect(role).toBeTruthy();
+      await createInstanceProfile(profileName, roleName);
+      profile = await instanceProfileExists(profileName);
+      expect(profile).toBeTruthy();
+    });
+    test('instance profile is not created if it exists', async () => {
+      let profile = await instanceProfileExists(profileName);
+      expect(profile).toBe(false);
+      const documentPolicy = await readJSONFile(
+        'ec2IamConfig',
+        join(__dirname, '..', 'src', 'templates'),
+      );
+      let role = await roleExists(roleName);
+      expect(role).toBe(false);
+      await createIamRole(documentPolicy, roleName, rolePolicies);
+      role = await roleExists(roleName);
+      expect(role).toBeTruthy();
+      await createInstanceProfile(profileName, roleName);
+      profile = await instanceProfileExists(profileName);
+      expect(profile).toBeTruthy();
+      await createInstanceProfile(profileName, roleName);
+      profile = await instanceProfileExists(profileName);
+      expect(profile).toBeTruthy();
+    });
+    test('instance profile is deleted correctly', async () => {
+      let profile = await instanceProfileExists(profileName);
+      expect(profile).toBe(false);
+      const documentPolicy = await readJSONFile(
+        'ec2IamConfig',
+        join(__dirname, '..', 'src', 'templates'),
+      );
+      let role = await roleExists(roleName);
+      expect(role).toBe(false);
+      await createIamRole(documentPolicy, roleName, rolePolicies);
+      role = await roleExists(roleName);
+      expect(role).toBeTruthy();
+      await createInstanceProfile(profileName, roleName);
+      profile = await instanceProfileExists(profileName);
+      await deleteInstanceProfile(profileName, roleName);
+      profile = await instanceProfileExists(profileName);
+      expect(profile).toBe(false);
+    });
   });
 });
